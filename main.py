9e6236dc1f0500f206d52fc91745c0126e3df41e
Aidan Dow <aidan@Aidans-MacBook-Pro.local>
2021-05-05 15:22:10 +0100

    Added command line funtionality

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-# Main
 import merger
 import os
+import sys
+from pathlib import Path
 
+DEFAULT_NAME = "OUTPUT_PDF"
 
-OUTPUT = '/Users/aidan/desktop'
-NAME = "OUTPUT_PDF"
+# Return the path of user's desktop
+def get_desktop():
+    return os.path.join(str(Path.home(),"Desktop")
+
+# Display basic help message
+def help():
+    print("Call pdf merger with:")
+    print('"python3 main.py <path> <fname>"')
+    print("-----------------------------------\n")
+    sys.exit()
+
+def handle_input():
+    try:
+        if sys.argv[1] in ('help', 'h', '-help', '--help', '-h', '--h', 'HELP', 'H'):
+            help()
+        # Set path to first command line argument
+        path = sys.argv[1]
+        
+    except IndexError:
+        # User has not specified a path, quit.
+        print("No path specified")
+        sys.exit()
+
+    # If user has specified a filename, store it, otherwise use default
+    try:
+        fname = sys.argv[2]
+    except IndexError:
+        fname = DEFAULT_NAME
+
+    return path, fname
 
 def main():
-    path = '/Users/aidan/downloads/pdfs'
-    destination = os.path.join(OUTPUT,NAME+".pdf")
-
+    path, filename = handle_input()
+    destination = os.path.join(get_desktop(),filename+".pdf")
     files = merger.getFiles(path)
     writer = merger.mergeFiles(files)
 
     with open(destination, 'wb') as f:
         writer.write(f)
-        f.close()
-
 
 if __name__ == "__main__":
     main()
--- merger.py
+++ merger.py
@@ -3,31 +3,27 @@ from os import listdir
 from os.path import join
 from os.path import exists
 
-# Recieve file path
-# Look for pdfs
-# read pdfs
-# add to common pdf
-
+# Returns a list of the filepaths for the pdfs in path.
 def getFiles(path):
     if not exists(path):
         raise FileNotFoundError
 
     return [join(path, f) for f in listdir(path) if isPDF(f)]
 
-
+# Adds each page in pdf to writer
 def addPdf(fname, writer):
     pdf = PdfFileReader(fname,'rb')
     for n in range(pdf.getNumPages()):
         writer.addPage(pdf.getPage(n))
 
-
+# Combines all of the pdfs in path_list
 def mergeFiles(path_list):
     writer = PdfFileWriter()
     for f in path_list:
         addPdf(f,writer)
     return writer
     
-
+# Returns a boolean denoting whether a file is a pdf
 def isPDF(f):
     return f.endswith(".pdf")
 
